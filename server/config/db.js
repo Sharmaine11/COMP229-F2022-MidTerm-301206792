5f27214b28f6044b9c88366917f8e0a0049cb081
Sharmaine <sapps1305@gmail.com>
2022-10-28 06:32:59 -0400

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //URI: "mongodb://127.0.0.1/emp",
+  URI: "mongodb+srv://sharmaine:meng17184JEAY@database.vwwsvmo.mongodb.net/Emp?retryWrites=true&w=majority"
+};
